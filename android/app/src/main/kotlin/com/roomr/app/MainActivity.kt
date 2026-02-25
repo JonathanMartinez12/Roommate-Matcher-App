19a66c77a447bf84abe58806f79932cef154ce8c
Jonathan Martinez <132180719+JonathanMartinez12@users.noreply.github.com>
2026-02-24 20:24:17 -0600

    Revert "Merge pull request #1 from JonathanMartinez12/jon/feature1.2 (#2)"
    This reverts commit 7be39c332efaa9b22223132f590208568d47bf3b.

--- android/app/src/main/kotlin/com/roomr/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.roomr.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('Placeholder test', (WidgetTester tester) async {
-    // TODO: Add widget tests after Firebase is configured
-    expect(true, isTrue);
-  });
-}
